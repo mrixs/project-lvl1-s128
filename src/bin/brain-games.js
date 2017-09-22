6e24d10cc2a36f678c30830d428f8ee518f3d13e
Vladimir Mrixs Zagainov <mrixs@mrixs.ru>
2017-09-23 01:37:40 +0300

    4th Step

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import * as index from '..';
+import greeting from '..';
 
 console.log('Welcome to the Brain Games!\n');
-index.greating();
+greeting();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
-export const greating = () => {
-  const actual = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${actual}!`);
+
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${name}!`);
 };
-export default greating;
