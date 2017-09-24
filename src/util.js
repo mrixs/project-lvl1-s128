0ec08a371b6d46a6d934e9df250f36c6ce82150b
Vladimir Mrixs Zagainov <mrixs@mrixs.ru>
2017-09-24 13:01:17 +0300

    Fixed GCD game logic

--- src/util.js
+++ src/util.js
@@ -1,6 +1,6 @@
 export const gcd = (num1, num2) => {
   const smallestNum = (num1 > num2) ? num2 : num1;
-  for (let gcdNum = 2; gcdNum <= smallestNum; gcdNum += 1) {
+  for (let gcdNum = smallestNum; gcdNum >= 1; gcdNum -= 1) {
     if ((num1 % gcdNum === 0) && ((num2 % gcdNum === 0))) {
       return gcdNum;
     }
